cb9f38ac0a434d2581d12619caf8a5246eab6839
marquinho89 <marcos_della@hotmail.com>
2022-11-24 19:37:57 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIAWOXWW6GT45KCWT24"
+  default = "AKIAWOXWW6GT45KCWT2411"
 }
 variable "secret_key" {
-    default = "eDNVe3Pm58Mc/gVSuUZuLt7T38u6DtqxrIxtuyL1"
+    default = "eDNVe3Pm58Mc/gVSuUZuLt7T38u6DtqxrIxtuyL111"
 }
 
 variable "aws_region" {
